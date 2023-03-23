5ec20fbb31ea97aa84875aed93ec528ad8e4b77e
xingxing <3552906110@qq.com>
2023-03-23 18:56:10 +0800

    add code

---
+++ src/main/java/com/ning/Git.java
@@ -0,0 +1,10 @@
+package com.ning;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("辛苦了，何宁生");
+        System.out.println("辛苦了，何宁生1");
+        System.out.println("辛苦了，何宁生2");
+        System.out.println("辛苦了，何宁生3");
+    }
+}
